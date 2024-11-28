ecb901685c0c26b2a7eec70775c68c5c20ba50a8
sellbell07@gmail.com <sellbell07@gmail.com>
2024-11-28 19:17:12 +0900

    asd

--- type.tsx
+++ type.tsx
@@ -50,12 +50,7 @@ const playerLynn: {
 이런 경우엔 Alas(별칭) 타입을 생성할 수 있음
 
  **************************************************************/
-// 별칭타입 생성
-type Age = number;
-type Player = {
-  name: string;
-  age?: Age; //이런 식으로도 사용 가능
-};
+
 
 // const nico : Player = {
 //   // 실제 값이 들어가는 곳
@@ -69,6 +64,14 @@ type Player = {
 // };
 
 // 함수에 타입선언 하는 방법
+
+// 별칭타입 생성
+type Age = number;
+type Player = {
+ readonly name: string; //readonly : ㄹㅇ 읽을 수만 있는 값, 수정 불가
+  age?: Age; //이런 식으로도 사용 가능 number or undefinde
+};
+
 function playerMaper(name:string): Player{
     return{
         name
@@ -76,3 +79,30 @@ function playerMaper(name:string): Player{
 }
 const nico = playerMaper("nico");
 nico.age = 12;
+
+
+// any를 쓰는 순간 typeScript의 기능 상실, any를 써서 타입스크립트의 보호 장치를 빠져나왔기 때문
+
+// TypeScript에서만 존재하는 타입
+
+// 어떤 타입인지 모르는 변수를 어떻게 해야될까?
+
+let a:unknown;
+// void : 빈값
+// naver : 함수가 절대 retrun하지 않음, 리턴하지 않고 에러를 발생시킬 때 사용
+
+// let b = a+1 // a type이 unknown이기 때문에 허용해주지 않음
+
+if(typeof a === 'number'){
+  let b = a+1 // 여기서는 허용됨, 왜? 들어온 a 의 타입은 number 니까
+}
+
+// 이런식으로도 사용 가능
+function hello(name:string|number){
+  if(typeof name === 'number'){
+    name 
+  }else if(typeof name === 'string'){
+    name 
+    
+  }
+}
